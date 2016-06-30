e0ec386a13caf9372ac422760f5ea89a49a4c4be
Diego Rodrigues <diego.mrodrigues@outlook.com>
2016-06-30 09:22:50 -0300

    I Love You Bebella

---
+++ www/js/controllers.js
@@ -0,0 +1,66 @@
+angular.module('app.controllers', [])
+  
+.controller('belezaCtrl', function($scope) {
+
+})
+   
+.controller('beleza2Ctrl', function($scope) {
+
+})
+   
+.controller('beleza3Ctrl', function($scope) {
+
+})
+   
+.controller('decoraOCtrl', function($scope) {
+
+})
+   
+.controller('decoraO2Ctrl', function($scope) {
+
+})
+   
+.controller('decoraO3Ctrl', function($scope) {
+
+})
+   
+.controller('roupasCtrl', function($scope) {
+
+})
+         
+.controller('homeCtrl', function($scope) {
+
+})
+   
+.controller('home2Ctrl', function($scope) {
+
+})
+   
+.controller('home3Ctrl', function($scope) {
+
+})
+   
+.controller('home4Ctrl', function($scope) {
+
+})
+   
+.controller('selagemCVinagreDeMaCtrl', function($scope) {
+
+})
+   
+.controller('hidratanteXCtrl', function($scope) {
+
+})
+   
+.controller('wishlistCtrl', function($scope) {
+
+})
+   
+.controller('minhaSacolaCtrl', function($scope) {
+
+})
+   
+.controller('meusPedidosCtrl', function($scope) {
+
+})
+ 
